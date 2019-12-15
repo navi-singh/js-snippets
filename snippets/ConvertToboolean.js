b8a415f1e16c0d2e030455de93779b6dc2e9f030
navi-singh <33882768+navi-singh@users.noreply.github.com>
2019-12-15 14:50:11 -0600

    Create ConvertToboolean.js

---
+++ snippets/ConvertToboolean.js
@@ -0,0 +1,6 @@
+const val = 10;
+var emptyVal;
+
+// Double negation (!!) will convert any object to boolean and will return false if nothing exists.
+console.log(`Check if val exist: ${!!val}`);
+console.log(`Check if emptyVal exist: ${!!emptyVal}`);
